90d34ebd77d13c36d72195daff272a703152f1ed
Andromeda-NGC224 <shadim115115@gmail.com>
2024-11-25 15:53:50 +0200

    clsx sidebar nav

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,22 +1,33 @@
+import clsx from 'clsx';
+import Link from 'next/link';
 import React from 'react';
 
 export interface SidebarItemProps {
+  current?: boolean;
   pathname: string;
   icon: React.ReactNode;
   children: React.ReactNode;
 }
 
 export default function SidebarItem({
+  current,
   pathname,
   children,
   icon,
 }: SidebarItemProps) {
   return (
     <li>
-      <a href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
+      <Link
+        href={pathname}
+        className={clsx(
+          'flex items-center h-9 mx-1 gap-3.5',
+          current &&
+            'after:h-full after:ml-auto after:border-2 after:border-purple-200 after:rounded-sm',
+        )}
+      >
         {icon}
         <span className="font-medium text-zinc-50">{children}</span>
-      </a>
+      </Link>
     </li>
   );
 }
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,11 +1,22 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import { ImExit } from 'react-icons/im';
 import { FaBriefcase } from 'react-icons/fa';
 import { HiSquares2X2 } from 'react-icons/hi2';
 import SidebarItem from './sidebar-item';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Sidebar() {
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const handleExitClick = () => {
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -18,21 +29,24 @@ export default function Sidebar() {
         />
         <ul className="space-y-7">
           <SidebarItem
+            current={pathname === '/dashboard'}
             pathname="/dashboard"
             icon={<HiSquares2X2 size={18} color="white" className="ml-5" />}
           >
-            {' '}
             Dashboard
           </SidebarItem>
           <SidebarItem
+            current={pathname === '/companies'}
             pathname="/companies"
             icon={<FaBriefcase size={18} color="white" className="ml-5" />}
           >
-            {' '}
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button
+          onClick={handleExitClick}
+          className="flex items-center gap-2 p-6 mt-auto mx-auto"
+        >
           <ImExit color="white" size={18} />
           <span className="font-medium text-white">Exit</span>
         </button>
